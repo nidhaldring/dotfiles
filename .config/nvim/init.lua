aba4cdb343266b24a6347e01fbb885c0e17c656c
nidhal <contact@nidhal.info>
2024-01-05 04:08:30 +0100

    map leader ff to git_files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -131,7 +131,7 @@ require("telescope").setup {
 
 -- telescope keybindings
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>ff", builtin.git_files, {})
 vim.keymap.set("n", "<leader>lg", builtin.live_grep, {})
 
 -- nerdtree keybindings
