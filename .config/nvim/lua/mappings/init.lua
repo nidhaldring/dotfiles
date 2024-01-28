8f2532fbc37ce57a15a6c57eb76cc37ca99a0b69
nidhal <contact@nidhal.info>
2024-01-28 22:09:06 +0100

    more lsp keybindings

--- .config/nvim/lua/mappings/init.lua
+++ .config/nvim/lua/mappings/init.lua
@@ -1,23 +1,23 @@
 local function map(mode, lhs, rhs, opts)
 	local options = { noremap = true, silent = true }
 	if opts then
-		options = vim.tbl_extend('force', options, opts)
+		options = vim.tbl_extend("force", options, opts)
 	end
 	vim.keymap.set(mode, lhs, rhs, options)
 end
 
 -- actual delete
-map("n", "<leader>d", "\"_d")
-map("v", "<leader>d", "\"_d")
-map("n", "c", "\"_c")
-map("v", "c", "\"_c")
-map("n", "C", "\"_C")
-map("v", "C", "\"_C")
+map("n", "<leader>d", '"_d')
+map("v", "<leader>d", '"_d')
+map("n", "c", '"_c')
+map("v", "c", '"_c')
+map("n", "C", '"_C')
+map("v", "C", '"_C')
 -- undo tree
-map('n', '<leader>ut', vim.cmd.UndotreeToggle)
+map("n", "<leader>ut", vim.cmd.UndotreeToggle)
 
 -- switch between two recent buffers
-map('n', '<leader><leader>', "<c-^>")
+map("n", "<leader><leader>", "<c-^>")
 
 -- toggle nvim-tree
 map("n", "<leader>ft", ":NvimTreeToggle<CR>")
@@ -27,6 +27,3 @@ local builtin = require("telescope.builtin")
 map("n", "<C-p>", builtin.find_files, {})
 map("n", "<leader>ff", builtin.find_files, {})
 map("n", "<leader>lg", builtin.live_grep, {})
-
--- format
-map("n", "<leader>fo", vim.lsp.buf.format)
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,24 +1,31 @@
 -- lsp config
-local lsp = require('lsp-zero').preset({})
+local lsp = require("lsp-zero").preset({})
 lsp.on_attach(function(client, bufnr)
 	-- see :help lsp-zero-keybindings
 	-- to learn the available actions
 	lsp.default_keymaps({ buffer = bufnr })
-	vim.keymap.set("n", "gh", function() vim.lsp.buf.hover() end, { buffer = bufnr, remap = false })
-	vim.keymap.set("n", "<leader>;", "<cmd> lua vim.diagnostic.open_float()<CR>", { buffer = bufnr, remap = false })
+	vim.keymap.set("n", "gh", function()
+		vim.lsp.buf.hover()
+	end, { buffer = bufnr, remap = false })
+
+	vim.keymap.set("n", "<leader>rn", function()
+		vim.lsp.buf.rename()
+	end, { buffer = bufnr, remap = false })
+
+	vim.keymap.set("n", "<leader>ca", function()
+		vim.lsp.buf.code_action()
+	end, { buffer = bufnr, remap = false })
 end)
 -- vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
 
 -- autocompletion keybindings
 -- You need to setup `cmp` after lsp-zero
-local cmp = require('cmp')
+local cmp = require("cmp")
 cmp.setup({
 	mapping = {
 		-- `Enter` key to confirm completion
-		['<CR>'] = cmp.mapping.confirm({ select = false }),
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
 	},
-	completion = { completeopt = 'menu,menuone,noinsert'}
+	completion = { completeopt = "menu,menuone,noinsert" },
 })
 lsp.setup()
-
-
