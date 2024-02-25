d6dfe2b65664e8943d91ed32b06b9dfa4aaf1f93
nidhal <contact@nidhal.info>
2024-02-25 21:25:29 +0100

    alert when battery is about to die

--- .config/i3blocks/scripts/battery.sh
+++ .config/i3blocks/scripts/battery.sh
@@ -7,6 +7,8 @@ then
 elif [ "$BATTERY_STATUS" = 'Discharging' ]
 then
 	echo "<span color='red'>$BATTERY_LVL $BATTERY_STATUS</span>"
+  BATTERY_LVL_NUM=$(echo $BATTERY_LVL | cut -d "%" -f 1)
+  [ $BATTERY_LVL_NUM -lt 10 ] && notify-send "For the love of god, please re-charge your pc!"
 else
 	echo "<span color='green'>$BATTERY_LVL $BATTERY_STATUS</span>"
 fi
