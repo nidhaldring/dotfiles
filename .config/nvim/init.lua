cbea96d23833560cc7d0ab1162f6ef808003d204
Nidhal Rnine <dringthedreamer@gmail.com>
2026-04-09 14:14:31 +0100

    migrate to lazy

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,18 +1,6 @@
 -- install plugins
-require("plugin-manager")
+-- require("plugin-manager")
+require("config.lazy")
 require("settings")
 require("plugins")
 require("mappings")
-
--- Force comment colors to grey after everything loads
-vim.api.nvim_create_autocmd("ColorScheme", {
-	pattern = "*",
-	callback = function()
-		vim.api.nvim_set_hl(0, "Comment", { fg = "#3e4451", italic = true })
-		vim.api.nvim_set_hl(0, "@comment", { fg = "#3e4451", italic = true })
-	end,
-})
-
--- Apply immediately
-vim.api.nvim_set_hl(0, "Comment", { fg = "#3e4451", italic = true })
-vim.api.nvim_set_hl(0, "@comment", { fg = "#3e4451", italic = true })
