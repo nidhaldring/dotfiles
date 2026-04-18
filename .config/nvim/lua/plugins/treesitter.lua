01c6df00b12e29733f20f425075f36a45c6ce778
nidhaldring <dringthedreamer@gmail.com>
2026-04-18 17:29:09 +0100

    upgrade nvim-treesitter to main branch

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,30 +2,34 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	lazy = false,
 	build = ":TSUpdate",
-	branch = "master",
+	branch = "main",
 	config = function()
-		local configs = require("nvim-treesitter.configs")
-		configs.setup({
-			highlight = {
-				ensure_installed = {
-					"c",
-					"lua",
-					"javascript",
-					"typescript",
-					"go",
-					"rust",
-					"vue",
-					"markdown",
-					"html",
-					"latex",
-					"yaml",
-					"json",
-				},
-				enable = true,
-				disable = function(_, bufnr)
-					return vim.api.nvim_buf_line_count(bufnr) > 50000
-				end,
-			},
+		require("nvim-treesitter").install({
+			"c",
+			"lua",
+			"javascript",
+			"typescript",
+			"go",
+			"rust",
+			"vue",
+			"html",
+			"yaml",
+			"json",
+			"markdown",
+      "markdown_inline"
+		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function(args)
+				if vim.api.nvim_buf_line_count(args.buf) > 50000 then
+					return
+				end
+				-- if we couldn't find a parser for filetype ignore parsing
+				if not pcall(vim.treesitter.start, args.buf) then
+					return
+				end
+				vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
 		})
 	end,
 }
